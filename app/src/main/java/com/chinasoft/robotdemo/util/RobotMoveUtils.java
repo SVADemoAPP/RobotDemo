1032cd3a5c91ed9db16754b38634d8220d016c1a
xuhaofu <915699084@qq.com>
2018-11-26 11:14:31 +0800

    1.提交机器人坐标和摇杆坐标转化

--- app/src/main/java/com/chinasoft/robotdemo/util/RobotMoveUtils.java
+++ app/src/main/java/com/chinasoft/robotdemo/util/RobotMoveUtils.java
@@ -1,5 +1,8 @@
 package com.chinasoft.robotdemo.util;
 
+import android.util.Log;
+import android.widget.Switch;
+
 import com.slamtec.slamware.AbstractSlamwarePlatform;
 
 /**
@@ -9,10 +12,56 @@ import com.slamtec.slamware.AbstractSlamwarePlatform;
 
 public class RobotMoveUtils {
     private AbstractSlamwarePlatform mPlatform;                                     //机器人控制平台
-    private static final int ROBOT_STOPING=0;                                       //机器人停止状态
-    private static final int ROBOT_GO_FORWORD=1;                                    //机器人向前状态
-    private static final int ROBOT_GO_BACK=-1;                                      //
-    private static final int ROBOT_GO_LEFT=-2;
-    private static final int ROBOT_GO_RIGHT=2;
+    private static final int ROBOT_STOPING = 0;                                       //机器人停止状态
+    private static final int ROBOT_GO_FORWORD = 1;                                    //机器人向前状态
+    private static final int ROBOT_GO_BACK = -1;                                      //
+    private static final int ROBOT_GO_LEFT = -2;
+    private static final int ROBOT_GO_RIGHT = 2;
+
+    /**
+     * 摇杆坐标系和机器人坐标系转换方法
+     */
+    private double cnTransformation(double angle) {
+        double rbRotationValue = 0f;
+        double rbAngle = 270f - angle;//获得 机器人坐标系下，机器人度数
+        if (rbAngle < 0) {
+            rbAngle += 360f;
+        }
+        if (rbAngle >= 0 && rbAngle <= 180) {
+            rbRotationValue = Math.toRadians(rbAngle);
+        } else if (rbAngle > 180 && rbAngle < 360) {
+            rbRotationValue = Math.toRadians(rbAngle) - 2 * Math.PI;
+        }
+        Log.e("XHF", "rbRotationValue=" + rbRotationValue);
+        return rbRotationValue;
+    }
+
+    /**
+     * 模糊判断机器人8个方向的运动
+     *
+     * @param angle
+     */
+    private void fuzzyDirection(double angle) {
+        double rbRotation = cnTransformation(angle);
+        if (rbRotation > -Math.PI / 8 && rbRotation < Math.PI / 8)                                                                          //判断控制机器人向上运动
+        {
+            Log.e("XHF","判断控制机器人向上运动");
+        } else if (rbRotation > Math.PI / 8 && rbRotation < Math.PI / 8 * 3) {                                                              //判断机器人向左上方45度角运动
+            Log.e("XHF","判断机器人向左上方45度角运动");
+        } else if (rbRotation > Math.PI / 8 * 3 && rbRotation < Math.PI / 8 * 5) {                                                          //判断机器人向左运动
+            Log.e("XHF","判断机器人向左运动");
+        } else if (rbRotation > Math.PI / 8 * 5 && rbRotation < Math.PI / 8 * 7) {                                                          //判断机器人向左下运动
+            Log.e("XHF","判断机器人向左下运动");
+        } else if ((rbRotation > Math.PI / 8 * 7 && rbRotation < Math.PI) || (rbRotation > -Math.PI && rbRotation < -Math.PI / 8 * 7)) {    //判断机器人向下运动
+            Log.e("XHF","判断机器人向下运动");
+        } else if (rbRotation > -Math.PI / 8 * 7 && rbRotation < -Math.PI / 8 * 5) {                                                        //判断机器人向右下运动
+            Log.e("XHF","判断机器人向右下运动");
+        } else if (rbRotation > -Math.PI / 8 * 5 && rbRotation < -Math.PI / 8 * 3) {                                                            //判断机器人向右移动
+            Log.e("XHF","判断机器人向右移动");
+        } else if (rbRotation > -Math.PI / 8 * 3 && rbRotation < -Math.PI / 8) {                                                              //判断机器人向右上运动
+            Log.e("XHF","判断机器人向右上运动");
+        }
+
+    }
 
 }
