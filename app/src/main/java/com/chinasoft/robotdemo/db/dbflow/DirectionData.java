44802e25b8eafa62f19a442bf746b0d03d6c97e2
xuhaofu <915699084@qq.com>
2018-12-05 10:50:19 +0800

    1.version1数据库简单包装

--- app/src/main/java/com/chinasoft/robotdemo/db/dbflow/DirectionData.java
+++ app/src/main/java/com/chinasoft/robotdemo/db/dbflow/DirectionData.java
@@ -10,9 +10,9 @@ import com.raizlabs.android.dbflow.annotation.Table;
  */
 
 @Table(database = AppDataBase.class)
-public class DirectionData extends BaseModel{
-//    @PrimaryKey(autoincrement = true)    //ID
-//    public long id;
+public class DirectionData extends BaseModel {
+    @PrimaryKey(autoincrement = true)    //ID
+    public long id;
     @Column
     public String mapName;
 
@@ -21,4 +21,58 @@ public class DirectionData extends BaseModel{
 
     @Column
     public String path;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public DirectionData() {
+
+    }
+
+    public DirectionData(String mapName, String directionName, String path) {
+        this.mapName = mapName;
+        this.directionName = directionName;
+        this.path = path;
+    }
+
+    public String getMapName() {
+        return mapName;
+    }
+
+    public void setMapName(String mapName) {
+        this.mapName = mapName;
+    }
+
+    public String getDirectionName() {
+        return directionName;
+    }
+
+    public void setDirectionName(String directionName) {
+        this.directionName = directionName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectionData{" +
+                "id=" + id +
+                ", mapName='" + mapName + '\'' +
+                ", directionName='" + directionName + '\'' +
+                ", path='" + path + '\'' +
+                '}';
+    }
+
+
 }
